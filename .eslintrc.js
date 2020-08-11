c40040b3244f954df2027acdeaaeecfea0d286e8
Shogo-Inamoto-DAC <shogo-inamoto@dac.co.jp>
2020-08-11 14:54:28 +0900

    Delete space-before-function-paren rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,7 @@ const config = {
   extends: ["kentcdodds", "kentcdodds/jest"],
   rules: {
     "max-len": "off",
-    "import/max-dependencies": "off",
-    "space-before-function-paren": [
-      "error",
-      {
-        anonymous: "never",
-        named: "never",
-        asyncArrow: "always"
-      }
-    ]
+    "import/max-dependencies": "off"
   }
 };
 
